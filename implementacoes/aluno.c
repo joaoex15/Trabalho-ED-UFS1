67d50d525a5b5ea106b27ee3ba99694d40968c39
Gabriel-carvalho04 <gabriel.be2004@gmail.com>
2024-08-15 22:43:15 -0300

    Criação das funções de matricular e remover aluno

---
+++ implementacoes/aluno.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "interfaces/classe.h"
+#include "../interfaces/aluno.h"
+
+struct classe
+{
+    uint8_t serie;
+    char turma;
+    char etapa[20];
+    uint32_t qtd_alunos;
+    char nome_professor[100];
+    struct classe *prox;
+    Aluno *alunos;
+};
+
+struct aluno
+{
+    int matricula;
+    char nome[40];
+    int idade;
+    struct aluno *proxAluno;
+    struct aluno *antAluno;
+};
+
+int matriculaAluno(
+    Classe **classes, 
+    int matricula, 
+    string nome, 
+    int idade,
+    uint8_t serie,
+    char turma,
+    string etapa,
+    string nome_professor) 
+{
+    Classe *classeAtual = *classes;
+    while (classeAtual != NULL) {
+        if (classeAtual->serie == serie &&
+            classeAtual->turma == turma &&
+            strcmp(classeAtual->etapa, etapa) == 0 &&
+            strcmp(classeAtual->nome_professor, nome_professor) == 0) {
+            break;
+        }
+        classeAtual = classeAtual->prox;
+    }
+
+    if (classeAtual == NULL)
+        return 1;
+
+    Aluno *novoAluno = (Aluno*) malloc(sizeof(Aluno));
+    if (novoAluno == NULL)
+        return 1;
+    
+    novoAluno->matricula = matricula;
+    novoAluno->idade = idade;
+    strcpy(novoAluno->nome, nome);
+    novoAluno->proxAluno = NULL;
+    novoAluno->antAluno = NULL;
+
+    if (classeAtual->alunos == NULL) {
+        classeAtual->alunos = novoAluno;
+    } else {
+        Aluno *alunoAtual = classeAtual->alunos;
+        while (alunoAtual->proxAluno != NULL) {
+            alunoAtual = alunoAtual->proxAluno;
+        }
+
+        alunoAtual->proxAluno = novoAluno;
+        novoAluno->antAluno = alunoAtual;
+    }
+
+    classeAtual->qtd_alunos++;
+    return 0;
+}
+
+
+int removeAluno(
+    Classe **classes, 
+    int matricula,
+    uint8_t serie,
+    char turma,
+    string etapa,
+    string nome_professor) 
+{
+    Classe *classeAtual = *classes;
+    while (classeAtual != NULL) {
+        if (classeAtual->serie == serie &&
+            classeAtual->turma == turma &&
+            strcmp(classeAtual->etapa, etapa) == 0 &&
+            strcmp(classeAtual->nome_professor, nome_professor) == 0) {
+            break;
+        }
+        classeAtual = classeAtual->prox;
+    }
+    
+    if (classeAtual == NULL || classeAtual->alunos == 0)
+        return 1;
+
+    Aluno *alunoAtual = classeAtual->alunos;
+
+    while (alunoAtual != NULL) {
+        if (alunoAtual->matricula == matricula) {
+            if (alunoAtual->antAluno != NULL)
+                alunoAtual->antAluno->proxAluno = alunoAtual->proxAluno;
+            else
+                classeAtual->alunos = alunoAtual->proxAluno;
+
+            if (alunoAtual->proxAluno != NULL)
+                alunoAtual->proxAluno->antAluno = alunoAtual->antAluno;
+
+            free(alunoAtual);
+            classeAtual->qtd_alunos--;
+            return 0;
+        }
+
+        alunoAtual = alunoAtual->proxAluno;
+    }
+    return 1;
+}
--- interfaces/aluno.h
+++ interfaces/aluno.h
@@ -5,4 +5,24 @@
 // Tipo abstrato Aluno
 typedef struct aluno Aluno;
 
+// Função que matricula um aluno em uma classe caso essa classe seja encontrada.
+int matriculaAluno(
+    Classe **classes, 
+    int matricula, 
+    string nome, 
+    int idade,
+    uint8_t serie,
+    char turma,
+    string etapa,
+    string nome_professor);
+
+// Função que remove um aluno de uma classe caso a classe seja encontrada, a quantidade de alunos da classe seja maior que 0 e o aluno seja encontrado. 
+int removeAluno(
+    Classe **classes, 
+    int matricula,
+    uint8_t serie,
+    char turma,
+    string etapa,
+    string nome_professor);
+
 #endif
